f20de5a11b0ef72fa2e56c5002552d4c288b2142
Dorzhu <arbaya@mail.ru>
2022-08-20 19:57:21 +0700

    try draw 4angle

--- src/main.cpp
+++ src/main.cpp
@@ -108,19 +108,33 @@ int main()
 
 	float vertices[] =
 	{
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
+		 0.5f,  0.5f, 0.0f,   //правав€ верхн€€
+		 0.5f, -0.5f, 0.0f,   //правав€ нижн€€
+		-0.5f,  0.5f, 0.0f,   //лева€ верхн€€
+		-0.5f, -0.5f, 0.0f    //лева€ нижн€€
+	};
+	
+	unsigned int indices[] = {
+		0, 1, 2,
+		1, 2, 3
 	};
 
 	unsigned int VBO, VAO;
 
+	glBindVertexArray(VAO);
+
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
 
-	glBindVertexArray(VAO);
+	//glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -139,14 +153,15 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(pWindow);
 		glfwPollEvents();
 	}
 
 	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &VBO); 
 
 	glfwTerminate();
 	return 0;
