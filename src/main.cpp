0dcd3fb3aad255a99816753dedb4bc3b86c79ec5
Dorzhu <arbaya@mail.ru>
2022-08-21 20:23:40 +0700

    Draw 2 triangles with echo VBO and VAO

--- src/main.cpp
+++ src/main.cpp
@@ -106,46 +106,46 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float vertices[] =
+	float firstTriangle[] =
 	{
 		-0.5f, -0.5f, 0.0f,   //
 		 0.0f, -0.5f, 0.0f,   //
-		-0.25f, 0.0f, 0.0f,   //
-	    //
+		-0.25f, 0.0f, 0.0f,   
+	};
+	float secondTriangle[] =
+	{
 		0.5f, -0.5f, 0.0f,
 		0.0f, -0.5f, 0.0f,
 		0.25f, 0.0f, 0.0f,
 	};
 	
-	unsigned int indices[] = {
+	/*unsigned int indices[] = {
 		0, 1, 2,
 		1, 2, 3
-	};
+	};*/
 
-	unsigned int VBO, VAO;
+	
+	unsigned int VBOs[2], VAOs[2];
 
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+	
+	//Первый треугольник
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);	
+	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(VAO);
-
-	//glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+	//втоорой тругольник
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, 0); //отвязка
-	glBindVertexArray(0);			  //отвязка
-
-
+	//----------------------------------------------------
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); отрисовка только линий треугольников, каркас
 
 	while (!glfwWindowShouldClose(pWindow))
 	{
@@ -153,21 +153,25 @@ int main()
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); отрисовка только линий треугольников, каркас
 		
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		/*glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-		//glBindVertexArray(0);
+		glBindVertexArray(0);*/
+
+		glBindVertexArray(VAOs[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(VAOs[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(pWindow);
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO); 
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
 
 	glfwTerminate();
 	return 0;
